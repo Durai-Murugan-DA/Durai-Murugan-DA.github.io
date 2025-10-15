c6bc5c809b16b6a3dd4a12e17d9a969a8f8855f4
Durai-Murugan-DA <duraijeeva2017@gmail.com>
2025-10-15 16:00:28 +0530

    Resolve merge conflicts: configure Vite base for GitHub Pages and replace workflow

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Personal-Portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
